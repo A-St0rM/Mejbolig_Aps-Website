54deb365c1eecd2a253060a9ccc7abb761267719
AlissaStorm <Alissaalidib@hotmail.com>
2025-03-07 19:13:33 +0100

    Updated the model classes

--- M.E.J-PropertyWebsite.Server/Models/PropertyPrice.cs
+++ M.E.J-PropertyWebsite.Server/Models/PropertyPrice.cs
@@ -6,9 +6,12 @@
         public int Deposit { get; set; }
         public int RentalPrice { get; set; }
         public double Aconto { get; set; }
+        public int RentalPropertyId { get; set; }
+        public int PropertyPriceId { get; set; }
 
-        public PropertyPrice(int deposit, int rentalPrice, double aconto)
+        public PropertyPrice(int RentalPropertyId, int deposit, int rentalPrice, double aconto)
         {
+            this.RentalPropertyId = RentalPropertyId;
             this.Deposit = deposit;
             this.RentalPrice = rentalPrice;
             this.Aconto = aconto;
