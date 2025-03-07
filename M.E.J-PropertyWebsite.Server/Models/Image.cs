03f0691ae4ee34f721274700223728d2c0cb6f95
AlissaStorm <Alissaalidib@hotmail.com>
2025-03-07 19:11:23 +0100

    Made two new tables and updated the model classes

---
+++ M.E.J-PropertyWebsite.Server/Models/Image.cs
@@ -0,0 +1,24 @@
+﻿namespace M.E.J_PropertyWebsite.Server.Models
+{
+    public class Image
+    {
+
+        public int ImageId { get; set; }
+        public string ImageUrl { get; set; }
+        public int RentalPropertyId { get; set; }
+
+        public RentalProperty RentalProperty { get; set; }
+
+        public Image(int imageId, string imageUrl, int rentalPropertyId)
+        {
+            this.ImageId = imageId;
+            this.ImageUrl = imageUrl;
+            this.RentalPropertyId = rentalPropertyId;
+        }
+
+        public Image()
+        {
+        }
+
+    }
+}
---
+++ M.E.J-PropertyWebsite.Server/Models/PropertyPrice.cs
@@ -0,0 +1,22 @@
+﻿namespace M.E.J_PropertyWebsite.Server.Models
+{
+    public class PropertyPrice
+    {
+
+        public int Deposit { get; set; }
+        public int RentalPrice { get; set; }
+        public double Aconto { get; set; }
+
+        public PropertyPrice(int deposit, int rentalPrice, double aconto)
+        {
+            this.Deposit = deposit;
+            this.RentalPrice = rentalPrice;
+            this.Aconto = aconto;
+        }
+
+        public PropertyPrice()
+        {
+        }
+
+    }
+}
