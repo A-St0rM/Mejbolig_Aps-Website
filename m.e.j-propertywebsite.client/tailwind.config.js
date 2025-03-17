b57520280009cd8b4d7415d6c89a3143e6ba322e
AlissaStorm <Alissaalidib@hotmail.com>
2025-03-17 15:22:23 +0100

    Removed tailwind prefligt

--- m.e.j-propertywebsite.client/tailwind.config.js
+++ m.e.j-propertywebsite.client/tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
+    corePlugins: {
+        preflight: false, // Deaktiverer Tailwind's reset
+    },
     content: [
-        './src/**/*.{js,jsx,ts,tsx}',
+        "./src/*/.{js,jsx,ts,tsx}", // Sikrer at alle JSX/TSX-filer er inkluderet
     ],
     theme: {
         extend: {},
     },
     plugins: [],
-}
+};
